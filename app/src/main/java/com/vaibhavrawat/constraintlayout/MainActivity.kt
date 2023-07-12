3b4e53a9dc4d08db317a8d87cc2ea33e84d754bd
Vaibhav Rawat <rawatvaibhav27@gmail.com>
2023-07-12 17:37:42 +0530

    ButtomUpdates

--- app/src/main/java/com/vaibhavrawat/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/vaibhavrawat/constraintlayout/MainActivity.kt
@@ -8,13 +8,19 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     var btnRelativeLayout : Button?= null
+    var btnRelativeLayoutSwitch : Button?=null
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnRelativeLayout = findViewById(R.id.btnRelativeLayout)
+        btnRelativeLayoutSwitch = findViewById(R.id.btnRelativeLayoutSwitch)
         btnRelativeLayout?.setOnClickListener {
-            var intent = Intent(this, ConstraintLayoutView::class.java)
+            val intent = Intent(this, ConstraintLayoutView::class.java)
+            startActivity(intent)
+        }
+        btnRelativeLayoutSwitch?.setOnClickListener {
+            val intent = Intent(this, RelativeLayout::class.java)
             startActivity(intent)
         }
     }
